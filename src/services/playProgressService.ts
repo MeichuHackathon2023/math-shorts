a8d972b6440f2986677a4a9ea63ca4e44ff9b1e8
austin030606 <austin030606@gmail.com>
2023-10-21 15:16:39 +0800

    feat: playProgress get/upsert Service

--- src/services/playProgressService.ts
+++ src/services/playProgressService.ts
@@ -3,8 +3,29 @@ import { prisma } from "@/utils/prisma";
 
 const playProgressService = {
     getPlayProgress: async ({ userId, videoId }: { userId: number; videoId: number}) => {
-        
+        const playProgress = await prisma.playProgress.findUnique({
+            where: { 
+                userId_videoId: {
+                    userId: userId,
+                    videoId: videoId,
+                },
+            },
+        });
+        return playProgress;
     },
+    updateOrCreatePlayProgress: async ({ userId, videoId, secondElapsed }: { userId: number, videoId: number, secondElapsed: number }) => {
+        const playProgress = await prisma.playProgress.upsert({
+            where: { 
+                userId_videoId: {
+                    userId: userId,
+                    videoId: videoId,
+                },
+            },
+            update: { secondElapsed: secondElapsed },
+            create: { userId: userId, videoId: videoId, secondElapsed: secondElapsed },
+        });
+        return playProgress;
+    }
 };
 
 export default playProgressService;
